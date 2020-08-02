5a0f6e7e46381a498fa05d488e249b57258f87c8
Jeff Gilliland <jgilly18@gmail.com>
2020-08-02 18:52:19 +0200

    get login working

--- apps/kid-money/src/app/app.component.ts
+++ apps/kid-money/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Kid } from '@entities';
 import { DataService } from '@lib/data';
+import { AuthService } from '@lib/app-shell';
 
 @Component({
 	selector: 'gilly-root',
@@ -8,67 +9,71 @@ import { DataService } from '@lib/data';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	constructor(private dataService: DataService) {}
+	mockKids = [
+		{
+			id: '1',
+			firstName: 'John',
+			lastName: 'Smith Jr.',
+			birthday: new Date('04/02/2009'),
+			gender: 'M',
+			money: 30.5
+		},
+		{
+			id: '2',
+			firstName: 'Julien',
+			lastName: 'Smith',
+			birthday: new Date('04/02/2009'),
+			gender: 'M',
+			money: 10
+		},
+		{
+			id: '3',
+			firstName: 'Kayla',
+			lastName: 'Smith',
+			birthday: new Date('04/02/2009'),
+			gender: 'F',
+			money: 459,
+			transactions: [
+				{
+					id: 1,
+					name: 'Bulldozer Toy',
+					type: 'Toy',
+					cost: 10,
+					createDate: new Date()
+				},
+				{
+					id: 2,
+					name: 'Broke the door handle',
+					type: 'Destruction',
+					cost: 100
+				},
+				{
+					id: 3,
+					name: 'Talk back to Mom',
+					type: 'Disrespect',
+					cost: 20
+				},
+				{
+					id: 4,
+					name: 'Hotwheels Cars',
+					type: 'Toy',
+					cost: 5
+				},
+				{
+					id: 5,
+					name: 'Skittles',
+					type: 'Candy',
+					cost: 1
+				}
+			]
+		}
+	];
+
+	constructor(private dataService: DataService, public auth: AuthService) {}
 
 	ngOnInit() {
-		this.dataService.setData(Kid, [
-			{
-				id: '1',
-				firstName: 'John',
-				lastName: 'Smith Jr.',
-				birthday: new Date('04/02/2009'),
-				gender: 'M',
-				money: 30.5
-			},
-			{
-				id: '2',
-				firstName: 'Julien',
-				lastName: 'Smith',
-				birthday: new Date('04/02/2009'),
-				gender: 'M',
-				money: 10
-			},
-			{
-				id: '3',
-				firstName: 'Kayla',
-				lastName: 'Smith',
-				birthday: new Date('04/02/2009'),
-				gender: 'F',
-				money: 459,
-				transactions: [
-					{
-						id: 1,
-						name: 'Bulldozer Toy',
-						type: 'Toy',
-						cost: 10,
-						createDate: new Date()
-					},
-					{
-						id: 2,
-						name: 'Broke the door handle',
-						type: 'Destruction',
-						cost: 100
-					},
-					{
-						id: 3,
-						name: 'Talk back to Mom',
-						type: 'Disrespect',
-						cost: 20
-					},
-					{
-						id: 4,
-						name: 'Hotwheels Cars',
-						type: 'Toy',
-						cost: 5
-					},
-					{
-						id: 5,
-						name: 'Skittles',
-						type: 'Candy',
-						cost: 1
-					}
-				]
-			}
-		]);
+		this.auth.userLoggedIn$.subscribe((loggedIn) => {
+			this.dataService.setData(Kid, this.mockKids);
+		});
 	}
 }
