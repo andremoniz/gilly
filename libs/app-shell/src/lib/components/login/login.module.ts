11c292ea9284cf89bdb29dd6a44c47f63b9d86d0
Jeff Gilliland <jgilly18@gmail.com>
2020-08-17 17:35:20 +0200

    fixing login component, integrating primeng

--- libs/app-shell/src/lib/components/login/login.module.ts
+++ libs/app-shell/src/lib/components/login/login.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 
 import { LoginComponent } from './login.component';
 
 @NgModule({
 	declarations: [LoginComponent],
-	imports: [CommonModule, FormsModule, ReactiveFormsModule, InputTextModule],
+	imports: [CommonModule, FormsModule, ReactiveFormsModule, InputTextModule, ButtonModule],
 	exports: [LoginComponent]
 })
 export class LoginModule {}
