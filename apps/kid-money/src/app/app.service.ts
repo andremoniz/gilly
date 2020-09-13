5d983057087f9c06a38a38db1f8cf8b043395dec
Jeff Gilliland <jgilly18@gmail.com>
2020-09-13 09:25:17 +0200

    positioning breadcrumb and cleaning up labeling

--- apps/kid-money/src/app/app.service.ts
+++ apps/kid-money/src/app/app.service.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { MenuItem } from 'primeng/api/menuitem';
 import { filter, map } from 'rxjs/operators';
 
+import { DataService } from '../../../../libs/data/src/lib/services/data/data.service';
+import { Kid } from '../../../../libs/entities/kid-money/kid';
 import { capitalizeFirstLetter } from './../../../../libs/utilities/src/lib/utilities/strings/capitalizeFirstLetter';
 
 @Injectable({ providedIn: 'root' })
@@ -10,7 +12,11 @@ export class AppService {
 	breadcrumbs: MenuItem[];
 	home: MenuItem;
 
-	constructor(private route: ActivatedRoute, private router: Router) {
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private dataService: DataService
+	) {
 		this.home = { icon: 'pi pi-home', routerLink: '/' };
 		this.breadcrumbs = [];
 
@@ -29,14 +35,21 @@ export class AppService {
 					.filter((p) => p && p !== '' && p !== 'home');
 
 				this.breadcrumbs = routerParts.map((part, i) => {
+					let label = capitalizeFirstLetter(part);
+
 					let routerLink = '';
 					for (let p = 0; p <= i; p++) {
 						routerLink += `/${routerParts[p]}`;
 					}
 
+					const activeKid: Kid = this.dataService.cache['Kid'].find((k) => k.id === part);
+					if (activeKid) {
+						label = activeKid.firstName;
+					}
+
 					return {
-						label: capitalizeFirstLetter(part),
-						routerLink: routerLink
+						label,
+						routerLink
 					};
 				});
 			});
